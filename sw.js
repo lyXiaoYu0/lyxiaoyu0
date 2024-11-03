a55ef8840de5296957f97f12e5142a66ed482d94
喜欢喝茶而已 <12443124+lyfdadwa@user.noreply.gitee.com>
2024-11-03 21:03:33 +0800

    Site updated: 2024-11-03 21:03:32

--- sw.js
+++ sw.js
@@ -6,7 +6,7 @@ const preCache = [
   "css/style.css",
   "js/script.js",
 ];
-const VERSION = "1730638995186";
+const VERSION = "1730639008999";
 const cacheDomain = [
   "fonts.googleapis.com",
   "npm.webcache.cn",
